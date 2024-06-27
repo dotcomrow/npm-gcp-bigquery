acfecbf0215cf460966a5de78b7d03dc910b19d8
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-06-27 16:01:08 -0400

    whoops
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import fetchAdapter from "@haverstack/axios-fetch-adapter";
 
 class GCloudBigquery {
   public static async query(projectId: string, keyfile: string, query: string) {
-    const scope = 'ttps://www.googleapis.com/auth/bigquery'; // replace with the desired scope
+    const scope = 'https://www.googleapis.com/auth/bigquery'; // replace with the desired scope
     
 
     const gcloudAuth = new GetAccessToken(keyfile);
