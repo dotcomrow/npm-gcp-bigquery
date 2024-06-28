93ec4f8fd74877331740b8f9f1a3128b392ea68f
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-06-27 21:05:51 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,5 +5,5 @@ const projectId = 'database-30f7f3c2abdbda4f';
 const keyFilePath = '/Users/admin/Downloads/database-30f7f3c2abdbda4f-80b680adee27.json'; // replace with the path to your service account key file
 const keyFileContent = fs.readFileSync(keyFilePath, 'utf8');
 
-console.log("asdf2")
+console.log("asdf3")
 console.log(GCloudBigquery.query(projectId, keyFileContent, 'SELECT * FROM `database-30f7f3c2abdbda4f.user_preferences`'))
