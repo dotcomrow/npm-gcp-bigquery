3f606dd245131f8f401ade084f09888cbd1377a6
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-17 06:09:41 -0400

    removing scope
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/GCPBigquery.ts
+++ src/GCPBigquery.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 export default class GCPBigquery {
   public static async query(projectId: string, accessToken: string, query: string) {
-    const scope = 'https://www.googleapis.com/auth/bigquery'; // replace with the desired scope
-    
     const response = await axios.post(
       `https://bigquery.googleapis.com/bigquery/v2/projects/${projectId}/queries`,
       {
