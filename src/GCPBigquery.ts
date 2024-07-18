6d6c15c5e356bb525c2badd8ce7d391d7ab11a42
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-18 11:17:21 -0400

    adding output formatting
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/GCPBigquery.ts
+++ src/GCPBigquery.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default class GCPBigquery {
-  public static async query(projectId: string, accessToken: string, query: string) {
+  public static async query(projectId: string, accessToken: string, query: string): Promise<any[]> {
     const response = await axios.post(
       `https://bigquery.googleapis.com/bigquery/v2/projects/${projectId}/queries`,
       {
@@ -15,6 +15,16 @@ export default class GCPBigquery {
       }
     );
 
-    return response.data;
+    var response_data: any[] | PromiseLike<any[]> = [];
+    if (response.data.totalRows > 0) {
+      for (var item of response.data.rows) {
+        var row: any = {};
+        for (var i = 0; i < item.f.length; i++) {
+          row[response.data.schema.fields[i].name] = item.f[i].v;
+        }
+        response_data.push(row);
+      }
+    } 
+    return response_data;
   }
 }
